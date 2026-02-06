5ded3b887c947ba78470058527a1bd9cd225f5a2
ALVIRA-ARIF\Arif <mohammad.arif@health.telstra.com>
2026-02-07 09:42:01 +1100

    register identity and application user

---
+++ BookShop.Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(50)]
+        public string? StreetAddress { get; set; }
+        [MaxLength(30)]
+        public string? City { get; set; }
+        [MaxLength(15)]
+        public string? State { get; set; }
+        [MaxLength(10)]
+        public string? PostCode { get; set; }
+    }
+}
