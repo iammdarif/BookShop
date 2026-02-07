9466c830e5f7736c66c8c294eb711ab47e9bc8f1
ALVIRA-ARIF\Arif <mohammad.arif@health.telstra.com>
2026-02-08 09:40:29 +1100

    Add identity roles and refactor Register page

--- BookShop.Utility/SD.cs
+++ BookShop.Utility/SD.cs
@@ -3,5 +3,9 @@ namespace BookShop.Utility
 {
     public static class SD
     {
+        public const string Role_Customer = "Customer";
+        public const string Role_Employee = "Employee";
+        public const string Role_Admin = "Admin";
+        public const string Role_Company = "Company";
     }
 }
--- BookShop/Program.cs
+++ BookShop/Program.cs
@@ -3,6 +3,8 @@ using BookShop.DataAccess.Repository;
 using BookShop.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BookShop.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +15,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+//builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+
+
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
 
 //builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 builder.Services.AddRazorPages();
 
